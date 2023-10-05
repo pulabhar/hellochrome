ca0f1e6853c5827b99c5705f1817ffbcc27fb4a1
pulabhar <145580063+pulabhar@users.noreply.github.com>
2023-10-05 09:39:37 +0530

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.bar([1,3,5,7,9],[5,2,7,8,2],label="data 1")
+plt.legend()
+plt.xlabel('x values')
+plt.ylabel('height')
+plt.title('vertical bar chart')
+plt.show()
